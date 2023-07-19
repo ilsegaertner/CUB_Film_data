10e2eeaa4ca9e55d0f53051c555539d946b73eb7
ilsegaertner <joachimpruegl@gmail.com>
2023-07-19 12:15:56 +0200

    Update

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
+const express = require("express"),
+  bodyParser = require("body-parser"),
+  uuid = require("uuid");
 
-  const express = require('express'), 
-    bodyParser = require('body-parser'),
-    uuid = require('uuid');
+const morgan = require("morgan");
+const app = express();
+const mongoose = require("mongoose");
+const Models = require("./models.js");
+const fs = require("fs");
 
-  const morgan = require('morgan');
-  const app = express ();
-  const mongoose = require('mongoose');
-  const Models = require('./models.js');
-  const fs = require('fs');
+const Movies = Models.Movie;
+const Users = Models.User;
+const Genres = Models.Genre;
+const Directors = Models.Director;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-  const Movies = Models.Movie;
-  const Users = Models.User;
-  const Genres = Models.Genre;
-  const Directors = Models.Director;
+mongoose.connect("mongodb://127.0.0.1/cfD", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({ extended: true }));
-
-  mongoose.connect('mongodb://localhost:27017/cfDB', { useNewUrlParser: true, useUnifiedTopology: true });
-
-  
-
-  app.use (morgan('common', {
-    stream: fs.createWriteStream('./log.txt', {flags: 'a'})
-  })); //the morgan token how it logs to the txt file
-  app.use(morgan('dev')); //the morgan token how it logs to the terminal
-  app.use (express.static('public')); //static files
-
+app.use(
+  morgan("common", {
+    stream: fs.createWriteStream("./log.txt", { flags: "a" }),
+  })
+); //the morgan token how it logs to the txt file
+app.use(morgan("dev")); //the morgan token how it logs to the terminal
+app.use(express.static("public")); //static files
 
 // NEW
 
@@ -42,38 +42,52 @@ We’ll expect JSON in this format
   Birthday: Date
 }
 */
-app.post('/users', (req, res) => {
-  Users.findOne( { Username: req.body.Username })
-  .then((user) => {
-    if (user) {
-      return res.status(400).send(req.body.Username + 'aready exists');
-    } else {
-      Users
-        .create({
+app.post("/users", (req, res) => {
+  Users.findOne({ Username: req.body.Username })
+    .then((user) => {
+      if (user) {
+        return res.status(400).send(req.body.Username + "aready exists");
+      } else {
+        Users.create({
           Username: req.body.Username,
           Password: req.body.Password,
           Email: req.body.Email,
-          Birthday: req.body.Birthday
+          Birthday: req.body.Birthday,
         })
-        .then((user) =>{res.status(201).json(user) })
-        .catch((error) => {
-          console.error(error);
-          res.status(500).send('Error: ' + error);
-        })
-    }
-  })
-  .catch((error) => {
-    console.error(error);
-    res.status(500).send('Error: ' + error);
-  });
+          .then((user) => {
+            res.status(201).json(user);
+          })
+          .catch((error) => {
+            console.error(error);
+            res.status(500).send("Error: " + error);
+          });
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send("Error: " + error);
+    });
 });
 
-
 // Get all users
-app.get('/users', (req, res) => {
+app.get("/users", (req, res) => {
   Users.find()
     .then((users) => {
       res.status(201).json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    });
+});
+
+
+
+// Get all movies
+app.get('/movies', (req, res) => {
+  Movies.find()
+    .then((movies) => {
+      res.status(201).json(movies);
     })
     .catch((err) => {
       console.error(err);
@@ -82,15 +96,17 @@ app.get('/users', (req, res) => {
 });
 
 
+
+
 // Get a user by username
-app.get('/users/:Username', (req, res) => {
-  Users.findOne( { Username: req.params.Username })
+app.get("/users/:Username", (req, res) => {
+  Users.findOne({ Username: req.params.Username })
     .then((user) => {
       res.json(user);
     })
     .catch((err) => {
       console.error(err);
-      res.status(500).send('Error: ' + err);
+      res.status(500).send("Error: " + err);
     });
 });
 
@@ -105,78 +121,64 @@ app.get('/users/:Username', (req, res) => {
   (required)
   Birthday: Date
 }*/
-app.put('/users/:Username', (req, res) => {
-  Users.findOneAndUpdate({ Username: req.params.Username },
-  { $set: { 
-      Username: req.body.Username,
-      Password: req.body.Password,
-      Email: req.body.Email,
-      Birthday: req.body.Birthday
-    }
-  },
-  { new: true }, // This line makes sure that the updated document is returned
-  (err, updatedUser) => {
-    if(err) {
-      console.error(err);
-      res.status(500).send('Error: ' + err);
-    } else {
-      res.json(updatedUser);
-    }
-  });
-});
-
-
-// Add a movie to a user's list of favorites
-app.post('/users/:Username/movies/:MovieID', (req, res) => {
-  Users.findOneAndUpdate({ Username: req.params.Username },
-  {
-      $push: { FavoriteMovies: req.params.MovieID }
+app.put("/users/:Username", (req, res) => {
+  Users.findOneAndUpdate(
+    { Username: req.params.Username },
+    {
+      $set: {
+        Username: req.body.Username,
+        Password: req.body.Password,
+        Email: req.body.Email,
+        Birthday: req.body.Birthday,
+      },
     },
     { new: true }, // This line makes sure that the updated document is returned
-  (err, updatedUser) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send('Error: ' + err);
-    } else {
-      res.json(updatedUser);
+    (err, updatedUser) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send("Error: " + err);
+      } else {
+        res.json(updatedUser);
+      }
     }
-  });
+  );
+});
+
+// Add a movie to a user's list of favorites
+app.post("/users/:Username/movies/:MovieID", (req, res) => {
+  Users.findOneAndUpdate(
+    { Username: req.params.Username },
+    {
+      $push: { FavoriteMovies: req.params.MovieID },
+    },
+    { new: true }, // This line makes sure that the updated document is returned
+    (err, updatedUser) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send("Error: " + err);
+      } else {
+        res.json(updatedUser);
+      }
+    }
+  );
 });
 
 // Delete a user by username
-app.delete('users/:Username', (req, res) => {
+app.delete("users/:Username", (req, res) => {
   Users.findOneAndRemove({ Username: req.params.Username })
     .then((user) => {
       if (!user) {
-        res.status(400).send(req.params.Username + ' was not found');
+        res.status(400).send(req.params.Username + " was not found");
       } else {
-        res.status(200).send(req.params.Username + ' was deleted');
+        res.status(200).send(req.params.Username + " was deleted");
       }
     })
     .catch((err) => {
       console.error(err);
-      res.status(500).send('Error: ' + err);
+      res.status(500).send("Error: " + err);
     });
 });
 
-
-/* OLD
-
-  app.get('/', (req, res) => {
-    res.send('Welcome to CUB FILM DATA!');
-  });
-
-  app.get('/movies', (req, res) => {
-    res.json(topMovies);
-  });
-
-  app.use((err, req, res, next) => {
-    console.error (err.stack);
-    res.status(500).send('something broke!');
-  });
-
-
-*/
-  app.listen(8080, () => {
-    console.log ('Your app is listening on port 8080.');
-  });
+app.listen(8080, () => {
+  console.log("Your app is listening on port 8080.");
+});
