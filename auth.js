201f49e0062901403d6710488dd1aa5916fb2d13
ilsegaertner <joachimpruegl@gmail.com>
2023-07-24 18:11:58 +0200

    Debug auth.js

--- auth.js
+++ auth.js
@@ -19,6 +19,7 @@ module.exports = (router) => {
   router.post('/login', (req,res) => {
     passport.authenticate('local', { session: false },
     (error, user, info) => {
+      if (error || !user) {
       return res.status(400).json({
         message: 'Something is not right',
         user: user
@@ -31,5 +32,6 @@ module.exports = (router) => {
       let token = generateJWTToken(user.toJSON());
       return res.json( { user, token }); // ES6 shorthand for res.json({ user: user, token: token})
     });
-  }) (req, res);
+  })(req, res);
+});
 }
